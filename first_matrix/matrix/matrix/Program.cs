82b936e65e13b39e25a79b9a7f6c2779e9bf0ed5
nik <nik@ma-il.ru>
2017-05-02 19:54:50 +0300

    c#

---
+++ first_matrix/matrix/matrix/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 0;
+            try
+            {
+                Console.WriteLine("Введите размер матрицы: ");
+                n = Convert.ToInt16(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            int[,] matrix = new int[n, n];
+            int[,] result = new int[n, n];
+            Random rnd = new Random();
+            //исходая симметрическая матрица размера Z
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i > 0 && j < i)
+                    {
+                        matrix[i, j] = matrix[j, i];
+                    }
+                    else
+                        matrix[i, j] = Convert.ToInt16(rnd.Next(0, 10).ToString());
+                }
+            }
+            //вывод на консоль
+            Console.Write("Исходная:\n");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\nРезультирующая:\n");
+            Result(n, matrix);
+            Result_v2(n, matrix);
+            Console.ReadKey();
+        }
+
+        public static void Result(int z, int[,] matrix)
+        {
+            int temp = 0;
+            int iter = 0;
+            int[,] result = new int[z, z];
+            for (int i = 0; i < z; i++)
+            {
+                for (int j = 0; j < z; j++)
+                {
+                    for (int k = 0; k + i < z && k + j < z; k++)
+                    {
+                        temp += matrix[k + i, k + j];
+                        iter++;
+                    }
+                    result[i, j] = temp;
+                    temp = 0;
+                }
+            }
+            Console.Write("Количество итераций:" + iter + "\n");
+            for (int i = 0; i < z; i++)
+            {
+                for (int j = 0; j < z; j++)
+                {
+                    Console.Write(result[i, j] + "\t");
+                }
+                Console.Write("\n");
+            }
+        }
+        public static void Result_v2(int z, int[,] matrix)
+        {
+            int temp = 0;
+            int iter = 0;
+            int[,] result = new int[z, z];
+            for (int i = 0; i < z; i++)
+            {
+                for (int j = 0; j < z; j++)
+                {
+                    if (i > 0 && j < i)
+                    {
+                        result[i, j] = result[j, i];
+                        iter++;
+                        continue;
+                    }
+                    for (int k = 0; k + i < z && k + j < z; k++)
+                    {
+                        temp += matrix[k + i, k + j];
+                        iter++;
+                    }
+                    result[i, j] = temp;
+                    temp = 0;
+                }
+            }
+            Console.Write("Количество итераций:" + iter + "\n");
+            for (int i = 0; i < z; i++)
+            {
+                for (int j = 0; j < z; j++)
+                {
+                    Console.Write(result[i, j] + "\t");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ second_multiplicate/multiplicate/multiplicate/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Polinom pol = new Polinom();
+            pol.Init();
+            Polinom pol2 = new Polinom();
+            pol2.Init();
+            Console.WriteLine(pol.ToString());
+            Console.WriteLine(pol2.ToString());
+            Console.WriteLine("Результирующий полином");
+            Polinom res = new Polinom();
+            res.n = pol.n + pol2.n;
+            res.koeff = new double[pol.n + pol2.n + 1];
+            for (int i = 0; i <= pol.n; i++)
+            {
+                for (int j = 0; j <= pol2.n; j++)
+                {
+                    res.koeff[i + j] += pol.koeff[i] * pol2.koeff[j];
+                }
+            }
+            Console.WriteLine(res.ToString());
+            Console.ReadKey();
+        }
+    }
+
+    class Polinom
+    {
+        public double[] koeff;
+        public int n;
+        public void Init()
+        {
+            Console.WriteLine("Степень полинома:");
+            n = Convert.ToInt16(Console.ReadLine());
+            koeff = new double[n + 1];
+            Console.WriteLine("Коэффициенты:");
+            for (int i = 0; i <= n; i++)
+            {
+                koeff[i] = Convert.ToInt16(Console.ReadLine());
+            }
+        }
+        public override string ToString()
+        {
+            string res = "";
+            for (int i = 0; i <= n; i++)
+            {
+                res += koeff[i] + "x^" + i + "+";
+            }
+            return res.Substring(0, res.Length - 1);
+        }
+    }
+}
